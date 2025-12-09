6775bbd4db7ee50f6293b453da473c8f387bc6f8
Shinnya01 <carreon.carll@gmail.com>
2025-12-09 15:40:41 +0800

    feat: added student task migration -mrc

---
+++ app/Models/StudentTask.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class StudentTask extends Model
+{
+    protected $fillable = [
+        "name","description","subject_id","due_date","","","",
+    ];
+}
